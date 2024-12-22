e5e9433997d7557f4e2f9807c6d6d418a4d5667b
Jesse Silva <jesse1eliseu@gmail.com>
2024-12-22 02:31:39 -0300

    setup email .env

--- projetecj/settings.py
+++ projetecj/settings.py
@@ -162,8 +162,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Email settings
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'jesse1eliseu@gmail.com'
-EMAIL_HOST_PASSWORD = 'otbr tnrg hlgl gbwu'
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = os.getenv('EMAIL_PORT')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS')
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
